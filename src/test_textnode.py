3cd2d35ead8d5fd9a5910de48d118b2d385cd3ea
cardboardmoon <jesse@jSurf>
2024-03-17 22:00:13 -0700

    missed saving test_textnode.py before init commit

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,7 +4,18 @@ from textnode import TextNode
 
 
 class TestTextNode(unittest.TestCase):
-    pass
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+    def test_none(self):
+        node = TextNode("texty", "italic")
+        node2 = TextNode("texty", "italic", "x.com")
+        self.assertEqual(node, node2)
+    def test_style(self):
+        node = TextNode("text", "italic")
+        node2 = TextNode("text", "underlined")
+        self.assertEqual(node, node2)
 
 if __name__ == "__main__":
     unittest.main()
